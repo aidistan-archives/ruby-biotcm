9b47d30ae1186d76d40c04bc4876bb815576e8f7
Aidi Stan <aidistan@live.com>
2014-03-16 15:42:22 +0800

    Add a benchmark DSL

--- bm/bm-helper.rb
+++ bm/bm-helper.rb
@@ -4,3 +4,12 @@ require 'benchmark'
 unless $:.include?(File.expand_path('../../lib', __FILE__))
   $:.unshift(File.expand_path('../../lib', __FILE__))
 end
+
+# Our benchmark DSL 
+def benchmark_of(title=nil, &block)
+  if title
+    puts ['=== '+ title + ' ===', '']
+  end
+  Benchmark.bmbm(&block)
+  puts
+end
