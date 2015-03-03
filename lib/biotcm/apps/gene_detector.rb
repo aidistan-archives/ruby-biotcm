13ee8bb60bcf991a76a94e2d44aae3f251be5ae8
Aidi Stan <aidistan@live.com>
2015-03-03 21:33:02 +0800

    Bump GeneDetector to v0.1.1

--- lib/biotcm/apps/gene_detector.rb
+++ lib/biotcm/apps/gene_detector.rb
@@ -3,24 +3,32 @@ require 'optparse'
 # A built-in app for gene detection
 class BioTCM::Apps::GeneDetector < BioTCM::Apps::App
   # Version of GeneDetector
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
   # Default patterns of genes to exclude
   DEFAULT_GENE_BLACKLIST = [
     '^\w$',
     '^\d'
   ]
-  # Default patterns of text to exclude
-  DEFAULT_TEXT_BLACKLIST = [
+  # Default patterns of text to transform
+  DEFAULT_TEXT_CHANGELIST = [
+    [/ type I receptor/i, 'R1'],
+    [/ type II receptor/i, 'R2'],
+    [/ R I/i, 'R1'],
+    [/ R II/i, 'R2'],
+    [/(\s*|-*)alpha/i, 'A'],
+    [/(\s*|-*)beta/i, 'B'],
+    [/(\s*|-*)gamma/i, 'G'],
+    [/(\s*|-*)kappa/i, 'K'],
   ]
 
   # Initialize a gene detector
   def initialize(
-    gene_blacklist:DEFAULT_GENE_BLACKLIST,
-    text_blacklist:DEFAULT_TEXT_BLACKLIST,
-    if_formalize:true
+    gene_blacklist: [],
+    text_changelist: [],
+    if_formalize: true
   )
-    @gene_regexp = gene_blacklist.empty? ? nil : Regexp.new('(' + gene_blacklist.join(')|(') + ')')
-    @text_regexp = text_blacklist.empty? ? nil : Regexp.new('(' + text_blacklist.join(')|(') + ')')
+    @gene_regexp = Regexp.new('(' + (DEFAULT_GENE_BLACKLIST + gene_blacklist).join(')|(') + ')')
+    @text_changelist = text_changelist
     @if_formalize = if_formalize
   end
   # Detect genes appearing in text
@@ -33,10 +41,13 @@ class BioTCM::Apps::GeneDetector < BioTCM::Apps::App
     unless @symbols
       @symbols = String.hgnc.symbol2hgncid.keys
       # Exclude symbol patterns
-      @symbols.reject! { |sym| sym =~ @gene_regexp } if @gene_regexp
+      @symbols.reject! { |sym| sym =~ @gene_regexp }
     end
-    # Exclude text patterns
-    text.gsub!(@text_regexp, ' ') if @text_regexp
+    # Transform text
+    (DEFAULT_TEXT_CHANGELIST + @text_changelist).each do |item|
+      text.gsub!(item[0], item[1])
+    end
+
     # Split sentences into words and eliminate redundancies
     rtn = text.split(/\.\s|\s?[,:!?#()\[\]{}]\s?|\s/).uniq & @symbols
     # Return approved symbols
