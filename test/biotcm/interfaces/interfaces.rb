591bb26938ae69a23924f9c9b7935d918c873d87
Aidi Stan <aidistan@live.com>
2015-04-02 16:39:26 +0800

    Add tests for interfaces

---
+++ test/biotcm/interfaces/interfaces.rb
@@ -0,0 +1,27 @@
+require_relative '../../test_helper'
+
+describe BioTCM::Interfaces do
+  describe BioTCM::Interfaces::Interface do
+    include BioTCM::Interfaces::Interface
+
+    it 'must render scripts' do
+      assert(render_template(File.expand_path('../template.R.erb', __FILE__), { name: 'world' }))
+    end
+  end
+
+  describe BioTCM::Interfaces::R do
+    include BioTCM::Interfaces::R
+
+    it 'must evaluate R scripts' do
+      assert(evaluate_r_script(File.expand_path('../template.R.erb', __FILE__), { name: 'world' }))
+    end
+  end
+
+  describe BioTCM::Interfaces::Matlab do
+    include BioTCM::Interfaces::Matlab
+
+    it 'must evaluate MATLAB scripts' do
+      assert(evaluate_matlab_script(File.expand_path('../template.m.erb', __FILE__), { name: 'world' }))
+    end
+  end
+end
