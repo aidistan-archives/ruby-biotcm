7ef6ac58495a2ba1a7b66b0b2363ba19899a28be
Aidi Stan <aidistan@live.com>
2015-09-06 18:06:43 +0800

    Update test/biotcm/apps/test_app.rb

--- test/biotcm/apps/test_app.rb
+++ test/biotcm/apps/test_app.rb
@@ -1,9 +1,4 @@
 require_relative '../../test_helper'
 
 describe BioTCM::Apps::App do
-  it 'must raise NotImplementedError' do
-    assert_raises(NotImplementedError) do
-      BioTCM::Apps::App.new.run
-    end
-  end
 end
